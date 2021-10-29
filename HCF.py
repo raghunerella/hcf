3fd2e9daebaa9fcc05f18d5c6e07f2c3e95d083a
raghunerella <69499662+raghunerella@users.noreply.github.com>
2021-10-29 11:37:32 +0530

    Add files via upload

---
+++ HCF.py
@@ -0,0 +1,12 @@
+"""n1=int(input("enter the number:"))
+n2=int(input("enter the number:"))
+if n1<n2:
+    small_number=n1
+else:
+    small_number=n2
+for i in range(1,small_number+1):
+    if (n1%i==0 and n2%i==0):
+        HCF=i
+print(HCF)"""
+
+
